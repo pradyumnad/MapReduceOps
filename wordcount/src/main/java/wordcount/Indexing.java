752a0559d4b09b452df59c9b7eb314eab4046ae7
pradyumnad <pradyumnadoddala@gmail.com>
2015-09-19 19:07:14 -0700

    Included indexing code and output.

--- wordcount/src/main/java/wordcount/Indexing.java
+++ wordcount/src/main/java/wordcount/Indexing.java
@@ -2,15 +2,21 @@ package wordcount;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -18,26 +24,25 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+//http://www.dcs.gla.ac.uk/~richardm/papers/IPM_MapReduce.pdf
+
 public class Indexing {
 
-	public static class TokenizerMapper extends
-			Mapper<Object, Text, Text, FloatWritable> {
+	private static String kDoc = "doc-id";
+	private static String kTf = "tf";
 
+	public static class TokenizerMapper extends
+			Mapper<Object, Text, Text, MapWritable> {
 		// private Text file = new Text();
 		private String fileName;
 		private static int words = 0;
 
-		private List<String> wordIndexes = new ArrayList<String>();
-
-		private Set<String> noOfDocs = new HashSet<String>();
-
 		@Override
 		protected void setup(
-				Mapper<Object, Text, Text, FloatWritable>.Context context)
+				Mapper<Object, Text, Text, MapWritable>.Context context)
 				throws IOException, InterruptedException {
 			fileName = ((FileSplit) context.getInputSplit()).getPath()
 					.toString();
-			noOfDocs.add(fileName);
 		}
 
 		public void map(Object key, Text value, Context context)
@@ -45,37 +50,45 @@ public class Indexing {
 			StringTokenizer itr = new StringTokenizer(value.toString());
 			while (itr.hasMoreTokens()) {
 				words++;
-				// file.set(fileName + "@" + itr.nextToken());
-				// context.write(file, new IntWritable(1));
-				wordIndexes.add(fileName + "@" + itr.nextToken());
+				String word = itr.nextToken().toLowerCase();
+				MapWritable map = new MapWritable();
+				map.put(new Text(kDoc), new Text(fileName));
+				map.put(new Text(kTf), new IntWritable(1));
+				context.write(new Text(word), map);
 			}
-		}
-
-		@Override
-		protected void cleanup(
-				Mapper<Object, Text, Text, FloatWritable>.Context context)
-				throws IOException, InterruptedException {
-			for (String entry : wordIndexes) {
-				context.write(new Text(entry), new FloatWritable(
-						(float) (1.0 / words)));
-			}
-
-			context.write(new Text("NoOfDocs"),
-					new FloatWritable(noOfDocs.size()));
 		}
 	}
 
 	public static class SearchReducer extends
-			Reducer<Text, FloatWritable, Text, FloatWritable> {
+			Reducer<Text, MapWritable, Text, ArrayWritable> {
 
-		public void reduce(Text key, Iterable<FloatWritable> values,
+		private Map<String, Integer> invertedIndexes = new HashMap<String, Integer>();
+
+		public void reduce(Text key, Iterable<MapWritable> values,
 				Context context) throws IOException, InterruptedException {
-			float tf = 0.0f;
-			for (FloatWritable val : values) {
-				tf += val.get();
+
+			ArrayWritable array = new ArrayWritable(Text.class);
+			for (MapWritable val : values) {
+				String docName = val.get(new Text(kDoc)).toString();
+
+				if (invertedIndexes.get(docName) != null) {
+					Integer count = invertedIndexes.get(docName);
+					invertedIndexes.put(docName, count.intValue() + 1);
+				} else {
+					invertedIndexes.put(docName, 0);
+				}
 			}
 
-			context.write(key, new FloatWritable(tf));
+			List<Text> indexes = new ArrayList<Text>();
+
+			for (String iterable_element : invertedIndexes.keySet()) {
+				indexes.add(new Text(iterable_element + " "
+						+ invertedIndexes.get(iterable_element)));
+			}
+
+			Text[] out = new Text[indexes.size()];
+			array.set(out);
+			context.write(new Text(key), array);
 		}
 	}
 
@@ -95,11 +108,11 @@ public class Indexing {
 		job.setReducerClass(SearchReducer.class);
 
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(FloatWritable.class);
+		job.setOutputValueClass(MapWritable.class);
 
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-		System.exit(job.waitForCompletion(true) ? 0 : 1);		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
